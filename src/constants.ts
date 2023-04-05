36cda0a9c2d50e9b9a451dcb4e5c32f7537db56c
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-04-05 17:24:15 -0300

    configure env

--- src/constants.ts
+++ src/constants.ts
@@ -16,38 +16,36 @@ export const ICONS: { [key: string]: string } = {
 };
 
 export const GRTPOOL_CONTRACT_ADDRESS =
-  '0xe91fc5f6cf045c83d265140abe5271e5600f820c';
-export const GRTSATELLITE_CONTRACT_ADDRESS: { [key: string]: string } = {
-  'eip155:5': '0xcb65e522f6e12091184fe41d6e34013ea620319a',
-  'eip155:97': '0xcB65E522F6e12091184fE41d6E34013ea620319a',
-  'eip155:338': '0x8822806A3c89DE7c7d01b05C85C54C92d7581846',
+  process.env.REACT_APP_GRTPOOL_CONTRACT_ADDRESS;
+
+export const GRTSATELLITE_CONTRACT_ADDRESS: { [key: string]: any } = {
+  'eip155:5': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_5,
+  'eip155:97': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_97,
+  'eip155:338': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_338,
 };
 
 export const DEPAY_DISPUTE_ADDRESS =
-  '0xef468d8bdE6Cab3e678587415226a3d4bAAc5F01';
+  process.env.REACT_APP_DEPAY_DISPUTE_ADDRESS;
 export const REALITY_CONTRACT_ADDRESS =
-  '0x6F80C5cBCF9FbC2dA2F0675E56A5900BB70Df72f';
+  process.env.REACT_APP_REALITY_CONTRACT_ADDRESS;
 
-export const GRT_CONTRACT_ADDRESS: { [key: string]: string } = {
-  'eip155:5': '0x1e3C935E9A45aBd04430236DE959d12eD9763162',
-  'eip155:97': '0x3b369B27c641637e5EE7FF9cF516Cb9F8F60cC85',
-  'eip155:338': '0xa6Ec5790C26102018b07817fd464E2673a5e2B8D',
+export const GRT_CONTRACT_ADDRESS: { [key: string]: any } = {
+  'eip155:5': process.env.REACT_APP_GRT_CONTRACT_ADDRESS_EIP155_5,
+  'eip155:97': process.env.REACT_APP_GRT_CONTRACT_ADDRESS_EIP155_97,
+  'eip155:338': process.env.REACT_APP_GRT_CONTRACT_ADDRESS_EIP155_338,
 };
 
-export const POOL_CONTRACT_ADDRESS: { [key: string]: string } = {
-  //'eip155:5': '0x97B434f5f0fc9Ab060918bBb68671bd614fEA6CE',
-  //'eip155:97': '0x97B434f5f0fc9Ab060918bBb68671bd614fEA6CE',
-  //'eip155:338': '0x2CD3728934dBac6f895421468Ff9B156d28f3aC7',
-  'eip155:97': '0xa49f85627F798C7962966a383847aeDF9AbE226c',
-  'eip155:5': '0x29e2b23FF53E6702FDFd8C8EBC0d9E1cE44d241A',
+export const POOL_CONTRACT_ADDRESS: { [key: string]: any } = {
+  'eip155:97': process.env.REACT_APP_POOL_CONTRACT_ADDRESS_EIP155_97,
+  'eip155:5': process.env.REACT_APP_POOL_CONTRACT_ADDRESS_EIP155_5,
 };
 
-export const DELIGHT_API_URL = 'https://delight-api.grindery.org';
+export const DELIGHT_API_URL = process.env.REACT_APP_DELIGHT_API_URL;
 
-export const TX_EXPLORER: { [key: string]: string } = {
-  'eip155:5': 'https://goerli.etherscan.io/tx/',
-  'eip155:97': 'https://testnet.bscscan.com/tx/',
-  'eip155:338': 'https://testnet.cronoscan.com/tx/',
+export const TX_EXPLORER: { [key: string]: any } = {
+  'eip155:5': process.env.REACT_APP_TX_EXPLORER_EIP155_5,
+  'eip155:97': process.env.REACT_APP_TX_EXPLORER_EIP155_97,
+  'eip155:338': process.env.REACT_APP_TX_EXPLORER_EIP155_338,
 };
 
 export const FAUCET_MENU = [
