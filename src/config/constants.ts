f9cd97b99a46ce87dd9faf968484b85ba81173cd
Egor Miliukov <milukove@gmail.com>
2023-04-09 04:23:54 +0300

    update evn variables

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,33 +1,8 @@
-export const SCREEN = {
-  TABLET: '768px',
-  TABLET_XL: '1024px',
-  DESKTOP: '1280px',
-  DESKTOP_XL: '1600px',
-};
-
-export const ICONS: { [key: string]: string } = {
-  DISCONNECT: '/images/icons/disconnect.svg',
-  CHECKBOX_CHECKED: '/images/icons/checkbox-checked.svg',
-  CHECKBOX_EMPTY: '/images/icons/checkbox-empty.svg',
-  COPY: '/images/icons/copy.svg',
-  SOCIAL_DISCORD: '/images/icons/social-discord.png',
-  SOCIAL_TG: '/images/icons/social-tg.png',
-  SOCIAL_TWITTER: '/images/icons/social-twitter.png',
-};
-
-export const GRTPOOL_CONTRACT_ADDRESS =
-  process.env.REACT_APP_GRTPOOL_CONTRACT_ADDRESS;
-
 export const GRTSATELLITE_CONTRACT_ADDRESS: { [key: string]: any } = {
   'eip155:5': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_5,
   'eip155:97': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_97,
   'eip155:338': process.env.REACT_APP_GRTSATELLITE_CONTRACT_ADDRESS_EIP155_338,
 };
-
-export const DEPAY_DISPUTE_ADDRESS =
-  process.env.REACT_APP_DEPAY_DISPUTE_ADDRESS;
-export const REALITY_CONTRACT_ADDRESS =
-  process.env.REACT_APP_REALITY_CONTRACT_ADDRESS;
 
 export const GRT_CONTRACT_ADDRESS: { [key: string]: any } = {
   'eip155:5': process.env.REACT_APP_GRT_CONTRACT_ADDRESS_EIP155_5,
@@ -84,3 +59,22 @@ export const FAUCET_MENU = [
     external: true,
   },
 ];*/
+
+// TODO: remove when components using this constants will be refactored
+export const SCREEN = {
+  TABLET: '768px',
+  TABLET_XL: '1024px',
+  DESKTOP: '1280px',
+  DESKTOP_XL: '1600px',
+};
+
+// TODO: remove when components using this constants will be refactored
+export const ICONS: { [key: string]: string } = {
+  DISCONNECT: '/images/icons/disconnect.svg',
+  CHECKBOX_CHECKED: '/images/icons/checkbox-checked.svg',
+  CHECKBOX_EMPTY: '/images/icons/checkbox-empty.svg',
+  COPY: '/images/icons/copy.svg',
+  SOCIAL_DISCORD: '/images/icons/social-discord.png',
+  SOCIAL_TG: '/images/icons/social-tg.png',
+  SOCIAL_TWITTER: '/images/icons/social-twitter.png',
+};
