67b0358796ba2e7d3fcf4e980933fd9e6b0b654c
Egor Miliukov <milukove@gmail.com>
2023-04-18 00:38:15 +0300

    fix history tests

--- cypress/e2e/8-history.cy.ts
+++ cypress/e2e/8-history.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Orders page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/buy/trade/history');
+    cy.visit('http://localhost:3000/history');
     cy.get('#connect-button').click();
     cy.acceptMetamaskAccess({
       allAccounts: false,
@@ -19,11 +19,6 @@ describe('Orders page', () => {
 
   it('Shows order history details', () => {
     cy.get('.OrderCard').should('exist');
-  });
-
-  it('Returns to trade page', () => {
-    cy.get('button.MuiIconButton-root').eq(1).click();
-    cy.get('.PageCardHeader__typography').first().should('have.text', 'Trade');
   });
 
   it('Show no orders history found', () => {
