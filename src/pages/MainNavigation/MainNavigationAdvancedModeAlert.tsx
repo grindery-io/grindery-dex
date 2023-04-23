4fb027888df2ae8914126e012ddbadc534184dd6
Egor Miliukov <milukove@gmail.com>
2023-04-23 23:24:10 +0300

    ability to switch off advanced mode from alert message

--- src/pages/MainNavigation/MainNavigationAdvancedModeAlert.tsx
+++ src/pages/MainNavigation/MainNavigationAdvancedModeAlert.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Alert } from '@mui/material';
+import { Alert, Switch, Tooltip } from '@mui/material';
 import {
   useAppSelector,
   selectUserAdvancedMode,
   selectUserAdvancedModeAlert,
   useAppDispatch,
+  setUserAdvancedMode,
+  selectUserId,
   setUserAdvancedModeAlert,
 } from '../../store';
+import { useUserController } from '../../controllers';
 
 type Props = {};
 
 const MainNavigationAdvancedModeAlert = (props: Props) => {
+  const userId = useAppSelector(selectUserId);
   const advancedMode = useAppSelector(selectUserAdvancedMode);
   const advancedModeAlert = useAppSelector(selectUserAdvancedModeAlert);
   const dispatch = useAppDispatch();
+  const { handleAdvancedModeToggleAction } = useUserController();
 
   return advancedMode && advancedModeAlert ? (
     <Alert
@@ -29,6 +34,31 @@ const MainNavigationAdvancedModeAlert = (props: Props) => {
     >
       Your are in advanced mode. It is safe to use but some information and
       features might not be working yet.
+      <Tooltip title="Disable advanced mode">
+        <Switch
+          checked={advancedMode}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            handleAdvancedModeToggleAction(userId, event.target.checked);
+          }}
+          color="success"
+          sx={{
+            marginRight: '0',
+            marginTop: '-15px',
+            marginBottom: '-15px',
+            marginLeft: '8px',
+            padding: '8px 10px',
+            height: '28px',
+            '& .MuiSwitch-switchBase': {
+              '&:hover': {
+                background: 'transparent !important',
+              },
+            },
+            '& .MuiSwitch-thumb': {
+              marginTop: '-5px',
+            },
+          }}
+        />
+      </Tooltip>
     </Alert>
   ) : null;
 };
